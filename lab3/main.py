a578f441409bc732447ad9f46574eaef18819ea9
Anastasiia Savchyn <sa@empat.tech>
2024-05-24 11:02:23 +0300

    min max optmizing

--- lab3/main.py
+++ lab3/main.py
@@ -10,8 +10,11 @@ class GeneticAlgorithm:
         self.num_parents = num_parents
         self.mutation_rate = mutation_rate
 
-        self.optimal_solution = 0
-        self.optimal_value = 0
+        self.optimal_solution_max = 0
+        self.optimal_value_max = 0
+
+        self.optimal_solution_min = 0
+        self.optimal_value_min = 0
     
     def func(self, x):
         return 5 * np.sin(10 * x) * np.sin(3 * x) / (x ** x)
@@ -22,7 +25,8 @@ class GeneticAlgorithm:
     def calculate_fitness(self, population):
         return self.func(population)
     
-    def select_best_individuals(self, population, fitness):
+    def select_best_individuals(self, population, fitness, reverse=False):
+        fitness = -fitness if reverse else fitness
         sorted_indices = np.argsort(fitness)
         return np.expand_dims(population[sorted_indices[-self.num_parents:]], axis=1)
     
@@ -44,11 +48,10 @@ class GeneticAlgorithm:
                 offspring_crossover[idx] = random_value
         return offspring_crossover
     
-    def optimize(self):
-        population = self.generate_population()
+    def form_population(self, population, reverse=False):
         for generation in range(self.num_generations):
             fitness = self.calculate_fitness(population)
-            parents = self.select_best_individuals(population, fitness)
+            parents = self.select_best_individuals(population, fitness, reverse=reverse)
             offspring_crossover = self.crossover(parents)
             offspring_mutation = self.mutate(offspring_crossover)
             parent_index = 0
@@ -60,17 +63,34 @@ class GeneticAlgorithm:
     
             print("Generation \tMin \t\tMax")
             print(f"{generation}\t\t{np.min(fitness):.5f}\t{np.max(fitness):.5f}")
-        self.optimal_solution = population[np.argmax(self.calculate_fitness(population))]
-        self.optimal_value = self.func(self.optimal_solution)
-        print("Y(x)max", self.optimal_value)
-        print("Xmax:", self.optimal_solution)
-        return self.optimal_solution, self.optimal_value
+        return population
+        
+    
+    def optimize(self):
+        population_max = self.generate_population()
+        population_max = self.form_population(population=population_max, reverse=False)
+        
+        self.optimal_solution_max = population_max[np.argmax(self.calculate_fitness(population_max))]
+        self.optimal_value_max = self.func(self.optimal_solution_max)
+        print("Y(x)max", self.optimal_value_max)
+        print("Xmax:", self.optimal_solution_max)
+
+        population_min = self.generate_population()
+        population_min = self.form_population(population=population_min, reverse=True)
+
+        self.optimal_solution_min = population_min[np.argmin(self.calculate_fitness(population_min))]
+        self.optimal_value_min = self.func(self.optimal_solution_min)
+        print("Y(x)min", self.optimal_value_min)
+        print("Xmin:", self.optimal_solution_min)
+
+        return self.optimal_solution_max, self.optimal_value_max, self.optimal_solution_min, self.optimal_value_min
     
     def plot_function(self):
         x = np.linspace(self.x_min, self.x_max, 1000)
         y = self.func(x)
         plt.plot(x, y, label='Y(x)=5*sin(10*x)*sin(3*x)/(x^x)')
-        plt.scatter(self.optimal_solution, self.optimal_value, color='red', label='Максимум')
+        plt.scatter(self.optimal_solution_max, self.optimal_value_max, color='red', label='Максимум')
+        plt.scatter(self.optimal_solution_min, self.optimal_value_min, color='green', label='Мінімум')
         plt.title('Графік функції')
         plt.xlabel('x')
         plt.ylabel('Y(x)')
@@ -81,11 +101,11 @@ class GeneticAlgorithm:
 
 x_min = 0
 x_max = 8
-num_generations = 100
+num_generations = 200
 population_size = 100
 num_parents = 20
 mutation_rate = 0.1
 
 ga = GeneticAlgorithm(x_min, x_max, num_generations, population_size, num_parents, mutation_rate)
-optimal_solution, optimal_value = ga.optimize()
+optimal_solution_max, optimal_value_max, optimal_solution_min, optimal_value_min = ga.optimize()
 ga.plot_function()
